65c7b67cde769b49fbaadfd9ef156b9f78fbca98
shonburgundy <seanbeloussow@yahoo.com>
2020-05-08 09:28:23 -0700

    add initialize method with 2 variables, both variables assigned attr writer method

--- lib/anagrams_antigrams.rb
+++ lib/anagrams_antigrams.rb
@@ -1,6 +1,12 @@
 class Compare
+  attr_writer(:wordOne, :wordTwo)
+  
+  def initialize(wordOne,wordTwo)
+    @wordOne = wordOne.downcase.split('')
+    @wordTwo = wordTwo.downcase.split('')
+  end
 
   def anagram
-    # code here
+    # compare two words by individual letter
   end  
 end  
